bd583e299d52d921c43bcf7d16848270c5f51fca
Levi Evans <levipierceevans@gmail.com>
2024-08-14 14:19:47 -0700

    ✨ (components.scss, index.html, menu.js): implement carousel feature for staff display to enhance user interface ♻️ (components.scss, index.html): refactor staff display structure for improved maintainability and consistency 🔧 (menu.js): update toggleMenu function to use let for block scoping to adhere to best practices

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  var menu = document.getElementById("main-menu");
+  let menu = document.getElementById("main-menu");
   if (menu.classList.contains("menu-visible")) {
     menu.classList.remove("menu-visible")
     menu.classList.add("menu-hidden")
@@ -8,5 +8,55 @@ function toggleMenu() {
     menu.classList.add("menu-visible")
   } else {
     menu.classList.add("menu-visible")
+  }
+}
+
+const carousels = document.getElementsByClassName("carousel-container");
+let carouselStates = [];
+
+
+
+
+for (let i = 0; i < carousels.length; i++) {
+  carouselStates.push({
+    activeIndex: 0,
+    items: carousels[i].getElementsByClassName("carousel-card"),
+  });
+}
+
+
+function carouselNext(carouselIndex) {
+  const carousel = carouselStates[carouselIndex];
+  if (carousel.activeIndex < carousel.items.length - 1) {
+    const nextIndex = carousel.activeIndex + 1;
+    carousel.items[nextIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    carousel.items[nextIndex].classList.add("carousel-card-active");
+    carousel.items[carousel.activeIndex].classList.remove("carousel-card-active");
+    carousel.activeIndex = nextIndex;
+  }
+  else {
+    const nextIndex = 0;
+    carousel.items[nextIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    carousel.items[nextIndex].classList.add("carousel-card-active");
+    carousel.items[carousel.activeIndex].classList.remove("carousel-card-active");
+    carousel.activeIndex = nextIndex;
+  }
+}
+
+function carouselPrevious(carouselIndex) {
+  const carousel = carouselStates[carouselIndex];
+  if (carousel.activeIndex > 0) {
+    const previousIndex = carousel.activeIndex - 1;
+    carousel.items[carousel.activeIndex].classList.remove("carousel-card-active");
+    carousel.items[previousIndex].classList.add("carousel-card-active");
+    carousel.items[previousIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    carousel.activeIndex = previousIndex;
+  }
+  else {
+    const previousIndex = carousel.items.length - 1;
+    carousel.items[carousel.activeIndex].classList.remove("carousel-card-active");
+    carousel.items[previousIndex].classList.add("carousel-card-active");
+    carousel.items[previousIndex].scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    carousel.activeIndex = previousIndex;
   }
 }
