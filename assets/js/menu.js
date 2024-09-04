75bb3d56922c904a68e77987e4de476a259b09fa
Levi Evans <levipierceevans@gmail.com>
2024-09-04 11:43:32 -0700

    begin to add support for the carousel buttons

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -14,16 +14,44 @@ function toggleMenu() {
 const carousels = document.getElementsByClassName("carousel-container");
 let carouselStates = [];
 
-let carousel = carouselStates[0];
-let slides = carousel.children;
-console.log(slides);
-
 for (let i = 0; i < carousels.length; i++) {
   carouselStates.push({
     activeIndex: 0,
-    items: carousels[i].getElementsByClassName("carousel-card"),
+    items: carousels[i].children,
   });
 }
+
+let carouselState = carouselStates[0];
+let carousel = carouselState.items;
+let slides = carousel[1].children;
+carousel[0].addEventListener("click", e => {
+  carouselState.activeIndex -= 1;
+  if (carouselState.activeIndex < 0) carouselState.activeIndex = carousel[1].children.length;
+  console.log(carouselState.activeIndex);
+  moveSlide(carouselState.activeIndex);
+});
+carousel[2].addEventListener("click", e => {
+  carouselState.activeIndex += 1;
+  if (carouselState.activeIndex < 0) carouselState.activeIndex = carousel[1].children.length;
+  console.log(carouselState.activeIndex);
+  moveSlide(carouselState.activeIndex);
+});
+function moveSlide(index) {
+
+}
+console.log("Carousel: " + carousel);
+console.log(slides)
+for (let element of slides) {
+  console.log("slides: " + element);
+}
+console.log("slides: " + slides);
+
+
+
+
+
+
+
 
 function carouselNext(carouselIndex) {
   const carousel = carouselStates[carouselIndex];
